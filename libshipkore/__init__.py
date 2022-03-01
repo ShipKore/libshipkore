b99d0ad08f69529e81045a9462db3aaa9e020e26
Gagandeep Singh <gagandeep.2020@gmail.com>
2022-03-02 03:04:28 +0530

    Bump version: 0.3.0 → 1.0.0

--- libshipkore/__init__.py
+++ libshipkore/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Innerkore"""
 __email__ = 'admin@innerkore.com'
-__version__ = '0.3.0'
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/shipkore/libshipkore',
-    version='0.3.0',
+    version='1.0.0',
     zip_safe=False,
 )
